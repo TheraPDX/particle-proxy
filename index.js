f1ae519ec5cae1b97286c9f6775eb664dc3a22d4
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-04-02 18:10:36 -0400

    added a status route to the proxy

--- index.js
+++ index.js
@@ -3,12 +3,33 @@ var bodyParser = require('body-parser');
 
 var app = express();
 var appPort = process.argv[2] || process.env.PORT || 8090;
+var proxyAuthToken = process.argv[3] || process.env.PROXY_AUTH_TOKEN;
+var particleAuthToken = process.argv[4] || process.env.PARTICLE_AUTH_TOKEN;
+
+var devices = [];
+var router = express.Router();
 
 app.use(bodyParser.json());
 
-app.all('*', (req, res) => {
-    res.status(200).json(req.headers.authorization);
+app.use('*', (req, res, next) => {
+    if (req.headers.authorization !== proxyAuthToken) {
+        res.status(403).json({ status: 403, message: 'No auth token found' });
+    }
+    else {
+        next();
+    }
 });
+
+router.get('/status', (req, res) => {
+    res.status(200).json({
+        status: '200',
+        proxyTokenFound: !!proxyAuthToken,
+        particleTokenFound: !!particleAuthToken,
+        devicesRegistered: devices.length
+    });
+});
+
+app.use('/api/v1', router);
 
 console.log('listening on port ' + appPort);
 app.listen(appPort);
