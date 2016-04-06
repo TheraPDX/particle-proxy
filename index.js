5bce2ed5a75dbc20eb8403bd2b9ffca5339e7427
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-04-05 21:12:12 -0400

    added cors to the server

--- index.js
+++ index.js
@@ -42,6 +42,12 @@ app.use('*', (req, res, next) => {
     }
 });
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 router.get('/status', (req, res) => {
     res.status(200).json({
         apiStatus: getApiStatus(),
